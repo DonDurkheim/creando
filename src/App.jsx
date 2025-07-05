13932d25af03584df9e8a7c10955410914656eb9
don_durkheim <don_durk@don.durkheim>
2025-07-05 15:44:26 +0200

    about us also done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import AboutUs from './components/AboutUs'
 
 const App = () => {
   return (
     <div className='flex flex-col'> 
       <Navbar />
       <Hero />
+      <AboutUs />
     </div>
   )
 }
